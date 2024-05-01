824d57383963f7c2e105c630dfd267880291d664
Erik Mabes <eam233@gmail.com>
2024-05-01 15:03:26 -0700

    create and list todos complete; styling edits; published

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 // from setup
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import "./App.css";
 
 // connecting frontend to api
-import { useState, useEffect } from 'react';
-import { generateClient } from 'aws-amplify/api';
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/api";
 
-import { createTodo } from './graphql/mutations';
-import { listTodos } from './graphql/queries';
-import { type CreateTodoInput, type Todo } from './API';
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import { type CreateTodoInput, type Todo } from "./API";
 
-const initialState: CreateTodoInput = { name: '', description: '' };
+// adding authentication
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Text,
+  View
+} from '@aws-amplify/ui-react';
+import { type AuthUser } from "aws-amplify/auth";
+import { type UseAuthenticator } from "@aws-amplify/ui-react-core";
+import "@aws-amplify/ui-react/styles.css";
+
+const initialState: CreateTodoInput = { name: "", description: "" };
 const client = generateClient();
 
-const App = () => {
+type AppProps = {
+  signOut?: UseAuthenticator["signOut"]; //() => void;
+  user?: AuthUser;
+};
+
+const App: React.FC<AppProps> = ({ signOut, user }) => {
   const [formState, setFormState] = useState<CreateTodoInput>(initialState);
   const [todos, setTodos] = useState<Todo[] | CreateTodoInput[]>([]);
 
@@ -30,7 +45,7 @@ const App = () => {
       const todos = todoData.data.listTodos.items;
       setTodos(todos);
     } catch (err) {
-      console.log('error fetching todos: ', err)
+      console.log("error fetching todos: ", err)
     }
   }
 
@@ -47,43 +62,48 @@ const App = () => {
         },
       });
     } catch (err) {
-      console.log('error creating todo: ', err)
+      console.log("error creating todo: ", err)
     }
-  } 
+  }
 
   return (
-    <div style={styles.container}>
-      <h2>Amplify Todos</h2>
-      <input 
-        onChange={(event) => 
+    <View style={styles.container}>
+      <Heading level={1} style={styles.Heading}>Hello {user ? user.username: 'Stranger'}</Heading>
+      <Button onClick={signOut} style={styles.signOut}>Sign out</Button>
+      <Heading level={2} style={styles.Heading}>Amplify Todos</Heading>
+      <input
+        onChange={(event) =>
           setFormState({ ...formState, name: event.target.value })
         }
         style={styles.input}
         value={formState.name}
         placeholder="Name"
       />
-      <input 
+      <input
         onChange={(event) =>
           setFormState({ ...formState, description: event.target.value })
         }
         style={styles.input}
         value={formState.description as string}
-        placeHolder="Description"
+        placeholder="Description"
       />
-      <button style={styles.button} onClick={addTodo}>
+      <Button style={styles.button} onClick={addTodo}>
         Create Todo
-      </button>
+      </Button>
       {todos.map((todo, index) => (
-        <div key={todo.id ? todo.id : index} style={styles.todo}>
-          <p style={styles.todoName}>{todo.name}</p>
-          <p style={styles.todoDescription}>{todo.description}</p>
-        </div>
+        <View key={todo.id ? todo.id : index} style={styles.todo}>
+          <Text style={styles.todoName}>{todo.name}</Text>
+          <Text style={styles.todoDescription}>{todo.description}</Text>
+        </View>
       ))}
-    </div>
+    </View>
   );
 };
 
 const styles = {
+  p: {
+    color:"white",
+  },
   container: {
     width: 400,
     margin: "0 auto",
@@ -100,8 +120,8 @@ const styles = {
     padding: 8,
     fontSize: 18,
   },
-  todoName: { fontSize: 20, fontWeight: "bold" },
-  todoDescription: { marginBottom: 0 },
+  todoName: { fontSize: 20, fontWeight: "bold", color: "#ddd" },
+  todoDescription: { marginBottom: 0, color: "#ccc" },
   button: {
     backgroundColor: "black",
     color: "white",
@@ -109,6 +129,9 @@ const styles = {
     fontSize: 18,
     padding: "12px 0px",
   },
+  Heading: {color: "#ddd"},
+  signOut: {backgroundColor: "#555", color: "#ddd"}
+
 } as const;
 
-export default App
+export default withAuthenticator(App);
